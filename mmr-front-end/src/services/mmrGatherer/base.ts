c0115cebd0a80f695d6aa899189f18fdc0d7695f
lucas <lucas.taillebourg@gmail.com>
2021-05-21 22:04:04 +0200

    Fix back url in FR

--- mmr-front-end/src/services/mmrGatherer/base.ts
+++ mmr-front-end/src/services/mmrGatherer/base.ts
@@ -18,7 +18,7 @@ import { Configuration } from "./configuration";
 // @ts-ignore
 import globalAxios, { AxiosPromise, AxiosInstance } from 'axios';
 
-export const BASE_PATH = "http://localhost:8080".replace(/\/+$/, "");
+export const BASE_PATH = "http://163.172.214.131:8083".replace(/\/+$/, "");
 
 /**
  *
